27940076a86d338c0fd00e246feef108c5abb54d
Evan Slaton <evanslaton@gmail.com>
2019-10-18 14:18:50 -0700

    Completed grains challenge

---
+++ csharp/acronym/Acronym.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append(Char.ToUpper(phrase[0]));
+
+        for (int i = 1; i < phrase.Length - 1; i++)
+        {
+            if ((phrase[i] == ' ' || phrase[i] == '-' || phrase[i] == '_') && 
+                (phrase[i + 1] != ' ' && phrase[i + 1] != '-' && phrase[i + 1] != '_'))
+            {
+                sb.Append(Char.ToUpper(phrase[i + 1]));
+            }
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public static class Grains
+{
+    private static int SQUARES_ON_CHESSBOARD = 64;
+
+    public static ulong Square(int n)
+    {
+        if (n < 1 || n > Grains.SQUARES_ON_CHESSBOARD) throw new ArgumentOutOfRangeException();
+        return (ulong)Math.Pow(2, n - 1);
+    }
+
+    public static ulong Total()
+    {
+        return (ulong)Math.Pow(2, Grains.SQUARES_ON_CHESSBOARD) - 1;
+    }
+}
