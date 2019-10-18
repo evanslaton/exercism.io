2736343297db0355b5ec6007421a047c21fd52d8
Evan Slaton <evanslaton@gmail.com>
2019-10-18 11:08:00 -0700

    Finished isogram exercise

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        HashSet<char> uniqueChars = new HashSet<char>();
+        int spacesAndUnderscoreCount = 0;
+
+        for (int i = 0; i < word.Length; i++) {
+            if (word[i] != ' ' && word[i] != '-')
+            {
+                uniqueChars.Add(Char.ToLower(word[i]));
+            }
+            else
+            {
+                spacesAndUnderscoreCount++;
+            }
+        }
+
+        return word.Length == uniqueChars.Count + spacesAndUnderscoreCount ? true : false;
+    }
+}
