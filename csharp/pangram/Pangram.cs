79e24ec5b0b67fae5873f4b96e53d815c4442d86
Evan Slaton <evanslaton@gmail.com>
2019-10-18 14:53:59 -0700

    Refactored pangram

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 
 public static class Pangram
 {
+    private static HashSet<char> LETTERS = new HashSet<char>(new char[] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'});
+
     public static bool IsPangram(string input)
     {
-        char[] letters = new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-        HashSet<char> lettersToCheckFor = new HashSet<char>(letters);
         HashSet<char> uniqueChars = new HashSet<char>();
 
         for (int i = 0; i < input.Length; i++) 
         {
-            if (lettersToCheckFor.Contains(Char.ToLower(input[i])))
+            if (Pangram.LETTERS.Contains(Char.ToLower(input[i])))
             {
                 uniqueChars.Add(Char.ToLower(input[i]));
             }
         }
-        return uniqueChars.Count == 26 ? true : false;
+
+        return uniqueChars.Count == Pangram.LETTERS.Count ? true : false;
     }
 }
