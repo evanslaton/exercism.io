75084b66407fa42ca0f3b148c5c58db48a36a546
Evan Slaton <evanslaton@gmail.com>
2019-10-20 13:13:36 -0700

    Refactored leap and saved

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -4,8 +4,7 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        if ((year % 4 == 0 && year % 100 != 0) ||
-            (year % 4 == 0 && year % 100 == 0 && year % 400 == 0))
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
         {
             return true;
         }
