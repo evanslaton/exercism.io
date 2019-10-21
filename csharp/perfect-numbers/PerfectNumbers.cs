daebf4dec1f300034e47db7ecd9a49de49575ce0
Evan Slaton <evanslaton@gmail.com>
2019-10-21 10:47:39 -0700

    Finshed perfect numbers challenge

---
+++ csharp/perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        if (number < 1) throw new ArgumentOutOfRangeException();
+        HashSet<int> factors = new HashSet<int>();
+        if (number != 1) factors.Add(1);
+
+        for (int i = 2; i <= number / 2; i++) 
+        {
+            if (number % i == 0) factors.Add(i);
+        }
+
+        int aliquotSum = 0;
+        foreach(int factor in factors)
+            aliquotSum += factor;
+
+        if (aliquotSum == number) 
+            return Classification.Perfect;
+        else if (aliquotSum > number) 
+            return Classification.Abundant;
+        else 
+            return Classification.Deficient;
+    }
+}
