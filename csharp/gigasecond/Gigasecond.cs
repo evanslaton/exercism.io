b06b54da8dc9b4de83339135f41bfd2cea195bfa
Evan Slaton <evanslaton@gmail.com>
2019-10-21 21:54:45 -0700

    Refactored to use AddSeconds instead of AddMilliseconds

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -2,5 +2,5 @@ using System;
 
 public static class Gigasecond
 {
-    public static DateTime Add(DateTime moment) => moment.AddMilliseconds(1_000_000_000_000);
+    public static DateTime Add(DateTime moment) => moment.AddSeconds(1_000_000_000);
 }
