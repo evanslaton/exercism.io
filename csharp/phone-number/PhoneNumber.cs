661869d5e877c1712799efd839f5a23a49372dcb
Evan Slaton <evanslaton@gmail.com>
2019-10-22 16:38:57 -0700

    Finished phone number challenge

---
+++ csharp/phone-number/PhoneNumber.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Text.RegularExpressions;
+
+public class PhoneNumber
+{
+    private static int MAX_LENGTH = 11;
+    
+    public static string Clean(string phoneNumber)
+    {
+        // Removes special characters
+        phoneNumber = Regex.Replace(phoneNumber, @"[^\d]", "");
+
+        // Validates phone number
+        Match match = Regex.Match(phoneNumber, @"^1?(2|3|4|5|6|7|8|9)\d{2}(2|3|4|5|6|7|8|9)\d{2}\d{4}$"); 
+
+        if (match.Success)
+        {
+            // If the country code is included, remove it before returning
+            return phoneNumber.Length == MAX_LENGTH ? phoneNumber.Substring(1) : phoneNumber;
+        }
+        else
+        {
+            throw new ArgumentException();
+        }
+    }
+}
