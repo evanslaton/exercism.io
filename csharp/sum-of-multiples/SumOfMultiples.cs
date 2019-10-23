4daca4c98c01184a8c2b5efafef46fb523612657
Evan Slaton <evanslaton@gmail.com>
2019-10-23 15:38:13 -0700

    Finished sum-of-multiples challenge

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class SumOfMultiples
+{
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        HashSet<int> uniqueMultiples = new HashSet<int>();
+
+        foreach (int number in multiples)
+        {
+            if (number == 0) continue;
+
+            for (int j = number; j < max; j++)
+            {
+                if (j % number == 0) uniqueMultiples.Add(j);
+            }
+        }
+
+        int sumOfMultiples = 0;
+        foreach (int number in uniqueMultiples) sumOfMultiples += number;
+
+        return sumOfMultiples;
+    }
+}
