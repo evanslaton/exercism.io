083386d2b7b1d7e2d2cb4254fd79c94c67cff455
Evan Slaton <evanslaton@gmail.com>
2019-10-24 15:32:04 -0700

    Refactored largest serires product challenge

--- csharp/largest-series-product/LargestSeriesProduct.cs
+++ csharp/largest-series-product/LargestSeriesProduct.cs
@@ -7,35 +7,24 @@ public static class LargestSeriesProduct
         if (span == 0) return 1;
         if (span > digits.Length || span < 0) throw new ArgumentException();
 
-        int[] numbers = ConvertStringToIntArray(digits);
         long largestProduct = Int64.MinValue;
-        for (int i = 0; i <= numbers.Length - span; i++)
+        for (int i = 0; i <= digits.Length - span; i++)
         {
-            largestProduct = Math.Max(largestProduct, GetProductOfSpan(numbers, span, i));
+            largestProduct = Math.Max(largestProduct, GetProductOfSpan(digits, span, i));
         }
         return largestProduct;
     }
 
-    private static int[] ConvertStringToIntArray(string digits)
-    {
-        int[] output = new int[digits.Length];
-        int number;
-        for (int i = 0; i < digits.Length; i++)
-        {
-            if (Int32.TryParse(digits[i].ToString(), out number)) 
-                output[i] = number;
-            else 
-                throw new ArgumentException();
-        }
-        return output;
-    }
-
-    private static long GetProductOfSpan(int[] numbers, int span, int startingIndex)
+    private static long GetProductOfSpan(string digits, int span, int startingIndex)
     {
         long product = 1;
+        int number;
         for (int i = startingIndex; i < startingIndex + span; i++)
         {
-           product *= numbers[i];
+            if (Int32.TryParse(digits[i].ToString(), out number)) 
+                product *= number;
+            else 
+                throw new ArgumentException();
         }
         return product;
     }
