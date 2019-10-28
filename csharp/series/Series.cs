401aa71d0317bca72f40e10c5b52a0dcf9ed00d7
Evan Slaton <evanslaton@gmail.com>
2019-10-28 11:05:31 -0700

    Finished series challenge

--- csharp/series/Series.cs
+++ csharp/series/Series.cs
@@ -4,6 +4,15 @@ public static class Series
 {
     public static string[] Slices(string numbers, int sliceLength)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (numbers.Length == 0 || sliceLength > numbers.Length || sliceLength < 1)
+            throw new ArgumentException();
+
+        string[] output = new string[numbers.Length - (sliceLength - 1)];
+        for (int i = 0; i < numbers.Length - (sliceLength - 1); i++)
+        {
+            output[i] = numbers.Substring(i, sliceLength);
+        }
+
+        return output;
     }
 }
