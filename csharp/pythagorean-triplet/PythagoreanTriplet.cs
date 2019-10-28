baf1aaf3f71cee1d3e50741f6624661de3f3a526
Evan Slaton <evanslaton@gmail.com>
2019-10-28 13:20:05 -0700

    Passing 4 of 7 tests in pythagorean triplets challenge

--- csharp/pythagorean-triplet/PythagoreanTriplet.cs
+++ csharp/pythagorean-triplet/PythagoreanTriplet.cs
@@ -5,6 +5,22 @@ public static class PythagoreanTriplet
 {
     public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<(int a, int b, int c)> output = new List<(int a, int b, int c)>();
+        int a, b, c = 0;
+        for (int n = 1; n < sum; n++) 
+        {
+            for (int m = n + 1; m < sum; m++)
+            {
+                //Euclid's Proof
+                a = m*m - n*n;
+                b = 2*m*n;
+                c = m*m + n*n;
+
+                if (a + b + c == sum && a < b) output.Add((a, b, c));
+                else if (a + b + c == sum && a > b) output.Add((b, a, c));
+            }
+
+        }
+        return output.ToArray();
     }
 }
