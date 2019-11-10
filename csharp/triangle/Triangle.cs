eb3524f44e5b7e8333976c58dd2d6eef1d09f358
Evan Slaton <evanslaton@gmail.com>
2019-11-09 21:02:23 -0800

    Finished triangle challenge

---
+++ csharp/triangle/Triangle.cs
@@ -0,0 +1,36 @@
+using System;
+
+public static class Triangle
+{
+    public static bool IsScalene(double side1, double side2, double side3)
+    {
+        if (!IsTriangle(side1, side2, side3)) return false;
+        return (side1 == side2 || side2 == side3 || side3 == side1) ? false : true;
+    }
+
+    public static bool IsIsosceles(double side1, double side2, double side3) 
+    {
+        if (!IsTriangle(side1, side2, side3)) return false;
+        return (side1 == side2 || side2 == side3 || side3 == side1) ? true : false;
+    }
+
+    public static bool IsEquilateral(double side1, double side2, double side3) 
+    {
+        if (!IsTriangle(side1, side2, side3)) return false;
+        return (side1 == side2 && side2 == side3) ? true : false;
+    }
+
+    private static bool IsTriangle(double side1, double side2, double side3)
+    {
+        if (!(side1 > 0) || !(side2 > 0) || !(side3 > 0))
+            return false;
+        else if (side1 + side2 < side3)
+            return false;
+        else if (side1 + side3 < side2)
+            return false;
+        else if (side2 + side3 < side1)
+            return false;
+        else
+            return true;
+    }
+}
