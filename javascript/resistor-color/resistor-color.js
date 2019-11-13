b3f2ab4cd9e84bdd9030a32425e90f134d15ab96
Evan Slaton <evanslaton@gmail.com>
2019-11-13 13:53:44 -0800

    Refactored resistor color

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -3,6 +3,8 @@
 // convenience to get you started writing code faster.
 //
 
-export const colorCode = (color) => COLORS.indexOf(color);
+const COLOR_MAP = {"black":0,"brown":1,"red":2,"orange":3,"yellow":4,"green":5,"blue":6,"violet":7,"grey":8,"white":9};
 
-export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
+export const colorCode = (color) => COLOR_MAP[color];
+
+export const COLORS = Object.keys(COLOR_MAP);
