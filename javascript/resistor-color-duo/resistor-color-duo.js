d20d280259be3a3619839b40f402a4bf14ca4607
Evan Slaton <evanslaton@gmail.com>
2019-11-13 14:10:45 -0800

    Finished resistor color duo challenge

---
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,7 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+const COLOR_MAP = {'black':'0','brown':'1','red':'2','orange':'3','yellow':'4','green':'5','blue':'6','violet':'7','grey':'8','white':'9'};
+
+export const value = (colors) => parseInt(COLOR_MAP[colors[0]] + COLOR_MAP[colors[1]]);
--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -3,7 +3,7 @@
 // convenience to get you started writing code faster.
 //
 
-const COLOR_MAP = {"black":0,"brown":1,"red":2,"orange":3,"yellow":4,"green":5,"blue":6,"violet":7,"grey":8,"white":9};
+const COLOR_MAP = {'black':0,'brown':1,'red':2,'orange':3,'yellow':4,'green':5,'blue':6,'violet':7,'grey':8,'white':9};
 
 export const colorCode = (color) => COLOR_MAP[color];
 
