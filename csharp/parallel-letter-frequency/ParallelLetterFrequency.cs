281dd1dab1d3cb353a077b846bbc3d9ffbd15c0b
Evan Slaton <evanslaton@gmail.com>
2019-11-13 12:52:01 -0800

    Finished parallel letter frequency challenge

---
+++ csharp/parallel-letter-frequency/ParallelLetterFrequency.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using System.Linq;
+
+public static class ParallelLetterFrequency
+{
+    public static Dictionary<char, int> Calculate(IEnumerable<string> texts)
+    {
+        ConcurrentDictionary<char, int> frequency = new ConcurrentDictionary<char, int>();
+        Parallel.ForEach(texts, text => GetFrequencies(text, frequency));
+        return frequency.ToDictionary(letter => letter.Key, letter => letter.Value);
+    }
+
+    private static void GetFrequencies(string text, ConcurrentDictionary<char, int> frequency) 
+    {
+        Parallel.ForEach(text, character =>
+        {
+            if (Char.IsLetter(character))
+                frequency.AddOrUpdate(Char.ToLower(character), 1, (key, value) => value + 1);
+        });
+    }
+}
