18238f31e1d62293cf1c4ef4fe50023ec9c9ab0c
Evan Slaton <evanslaton@gmail.com>
2019-11-14 08:19:08 -0800

    Refactored transistor color duo to have O(1) time

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -2,6 +2,17 @@
 // This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-const COLOR_MAP = {'black':'0','brown':'1','red':'2','orange':'3','yellow':'4','green':'5','blue':'6','violet':'7','grey':'8','white':'9'};
 
-export const value = (colors) => parseInt(COLOR_MAP[colors[0]] + COLOR_MAP[colors[1]]);
+const COLOR_MAP = new Map();
+  COLOR_MAP.set('black', '0');
+  COLOR_MAP.set('brown', '1');
+  COLOR_MAP.set('red', '2');
+  COLOR_MAP.set('orange', '3');
+  COLOR_MAP.set('yellow', '4');
+  COLOR_MAP.set('green', '5');
+  COLOR_MAP.set('blue', '6');
+  COLOR_MAP.set('violet', '7');
+  COLOR_MAP.set('grey', '8');
+  COLOR_MAP.set('white', '9');
+
+export const value = (colors) => parseInt(COLOR_MAP.get(colors[0]) + COLOR_MAP.get(colors[1]));
