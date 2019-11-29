961bccb6c55e27bf818d5bf688371585161fc827
Evan Slaton <evanslaton@gmail.com>
2019-11-29 14:07:52 -0800

    Finished collatz conjecture challenge

---
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -0,0 +1,17 @@
+import { formatResultsErrors } from "jest-message-util";
+
+//
+// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const steps = (n) => {
+  if (n <= 0) throw new Error('Only positive numbers are allowed');
+  let count = 0;
+  while (n !== 1) {
+    if (n % 2 == 0) n /= 2;
+    else n =  n * 3 + 1;
+    count++;
+  }
+  return count;
+};
