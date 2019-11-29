890cb84716d4c929781c43cc125249ef62147912
Evan Slaton <evanslaton@gmail.com>
2019-11-29 14:22:42 -0800

    Working on word count

---
+++ javascript/word-count/word-count.js
@@ -0,0 +1,21 @@
+//
+// This is only a SKELETON file for the 'Word Count' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Words {
+  count(inputString) {
+    const words = inputString.trim().toLowerCase().split(' ');
+    const output = {};
+    words.forEach(word => {
+      if (word !== '') {
+        if (word in output) {
+          output[word] = output[word] + 1;
+        } else {
+          output[word] = 1;
+        }
+      }
+    });
+    return output;
+  }
+}
