8b2f3d6b68b9c2cdc2ece951663da7772ba14f1f
Evan Slaton <evanslaton@gmail.com>
2019-12-04 10:20:13 -0800

    Finished difference of squares challenge

---
+++ javascript/difference-of-squares/difference-of-squares.js
@@ -0,0 +1,30 @@
+//
+// This is only a SKELETON file for the 'Difference of Squares' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Squares {
+  constructor(number) {
+    this.number = number;
+  }
+
+  get sumOfSquares() {
+    let sum = 0;
+    for (let i = 1; i <= this.number; i++) {
+      sum += i * i;
+    }
+    return sum;
+  }
+
+  get squareOfSum() {
+    let sum = 0;
+    for (let i = 1; i <= this.number; i++) {
+      sum += i;
+    }
+    return sum * sum;
+  }
+
+  get difference() {
+    return this.squareOfSum - this.sumOfSquares;
+  }
+}
