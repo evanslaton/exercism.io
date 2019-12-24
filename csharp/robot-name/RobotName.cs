c9b207b6ab09f3965b2673ee54abcb7215df6d77
Evan Slaton <evanslaton@gmail.com>
2019-12-24 10:34:44 -0800

    Refactored robot name challenge per mentor feedback

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 
 public class Robot
 {
-    private static char[] LETTERS = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-    private static int NUMBER_UPPER_LIMIT = 999;
-    private static HashSet<string> robotNames = new HashSet<string>();
-    public string name;
+    private const string LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private const int NUMBER_UPPER_LIMIT = 999;
+    private const int LEFT_PADDING = 3;
+    private const char PADDING_CHAR = '0';
+    private readonly static HashSet<string> robotNames = new HashSet<string>();
+    private readonly static Random RANDOM = new Random();
+    private string name;
 
     public string Name
     {
@@ -14,7 +17,7 @@ public class Robot
         {
             return name;
         }
-        set
+        private set
         {
             name = value;
         }
@@ -30,7 +33,7 @@ public class Robot
        string newName;
         do
         {
-            newName = GetTwoLetters() + GetThreeDigits();
+            newName = $"{GetTwoLetters()}{GetThreeDigits()}";
         
         } while (newName == Name || robotNames.Contains(newName));
         robotNames.Remove(Name);
@@ -40,23 +43,15 @@ public class Robot
 
     private static string GetTwoLetters()
     {
-        Random random = new Random();
-        int firstNumber = random.Next(0, LETTERS.Length);
-        int secondNumber = random.Next(0, LETTERS.Length);
+        int firstNumber = RANDOM.Next(0, LETTERS.Length);
+        int secondNumber = RANDOM.Next(0, LETTERS.Length);
         return LETTERS[firstNumber].ToString() + LETTERS[secondNumber].ToString();
     }
 
     private static string GetThreeDigits() 
     {
-        Random random = new Random();
-        int randomNumber = random.Next(0, NUMBER_UPPER_LIMIT);
-        string randomNumberAsString;
-        if (randomNumber < 10) 
-            randomNumberAsString = "00" + randomNumber;
-        else if (randomNumber < 100) 
-            randomNumberAsString = "0" + randomNumber;
-        else 
-            randomNumberAsString = randomNumber.ToString();
-        return randomNumberAsString;
+        return RANDOM.Next(0, NUMBER_UPPER_LIMIT)
+            .ToString()
+            .PadLeft(LEFT_PADDING, PADDING_CHAR);
     }
 }
